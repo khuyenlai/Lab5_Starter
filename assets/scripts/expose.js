0d7e22668d95d4410b0dec14d7da2cafe69be3f5
Khuyen Lai <145186270+khuyenlai@users.noreply.github.com>
2025-05-07 02:04:46 -0700

    Finish Lab 5 expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,42 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const hornSelect = document.getElementById('horn-select');
+  const hornImage = document.querySelector('img'); // only one main <img> tag
+  const hornAudio = document.querySelector('audio');
+  const volumeSlider = document.getElementById('volume');
+  const volumeIcon = document.querySelector('#volume-controls img');
+  const playButton = document.querySelector('button');
+
+  console.log({ hornSelect, hornImage, hornAudio, volumeSlider, volumeIcon, playButton });
+  hornSelect.addEventListener('change', () => {
+    const hornType = hornSelect.value;
+    hornImage.src = `assets/images/${hornType}.svg`;
+    hornAudio.src = `assets/audio/${hornType}.mp3`;
+  });
+  
+  volumeSlider.addEventListener('input', () => {
+    const volumeValue = volumeSlider.value;
+    hornAudio.volume = volumeValue / 100;
+  
+    if (volumeValue == 0) {
+      volumeIcon.src = 'assets/icons/volume-level-0.svg';
+    } else if (volumeValue < 33) {
+      volumeIcon.src = 'assets/icons/volume-level-1.svg';
+    } else if (volumeValue < 67) {
+      volumeIcon.src = 'assets/icons/volume-level-2.svg';
+    } else {
+      volumeIcon.src = 'assets/icons/volume-level-3.svg';
+    }
+  });
+  
+  playButton.addEventListener('click', () => {
+    hornAudio.play();
+  
+    if (hornSelect.value === 'party-horn') {
+      const jsConfetti = new JSConfetti();
+      jsConfetti.addConfetti();
+    }
+  });
+  
 }
